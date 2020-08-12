560d31f7eb443706205330bf4847e91c936934f1
dwrolvink <dwrolvink@gmail.com>
2020-08-12 13:10:18 +0200

    new start diagram

--- sets/rectangles1.js
+++ sets/rectangles1.js
@@ -1,7 +1,31 @@
 
 obj = newRect(ObjectList.objects);
+obj.pos.y = 97;
+obj.pos.x = 104;
+obj.width = 38;
+obj.height = 16;
+obj.bgcolor = 'rgba(13,5,50,1)';
+obj.text = "   Backend.Vault";
+obj.textsize = 8;
+obj.draw_arrow = "none";
+obj.border_radius = "30";
+
+
+obj = newRect(ObjectList.objects);
+obj.pos.y = 97;
+obj.pos.x = 74;
+obj.width = 28;
+obj.height = 28;
+obj.bgcolor = 'rgba(13,5,50,1)';
+obj.text = "   Integrator";
+obj.textsize = 8;
+obj.draw_arrow = "none";
+obj.border_radius = "30";
+
+
+obj = newRect(ObjectList.objects);
 obj.pos.y = 11;
-obj.pos.x = 49;
+obj.pos.x = 50;
 obj.width = 76;
 obj.height = 52;
 obj.bgcolor = 'rgba(13,5,50,1)';
@@ -65,8 +89,8 @@ obj.pos.x = 78;
 obj.width = 14;
 obj.height = 6;
 obj.bgcolor = 'rgba(14,75,213,1)';
-obj.text = "Get_Decrypt_Key (secret)";
-obj.textsize = 6;
+obj.text = "Get_Decrypt_Key";
+obj.textsize = 7;
 obj.draw_arrow = "none";
 obj.border_radius = "0";
 
@@ -113,8 +137,8 @@ obj.pos.x = 78;
 obj.width = 14;
 obj.height = 8;
 obj.bgcolor = 'rgba(14,75,213,1)';
-obj.text = "Get_Secret (uuid)";
-obj.textsize = 6;
+obj.text = "Get_Secret";
+obj.textsize = 8;
 obj.draw_arrow = "none";
 obj.border_radius = "0";
 
@@ -136,7 +160,7 @@ obj.pos.y = 23;
 obj.pos.x = 110;
 obj.width = 10;
 obj.height = 6;
-obj.bgcolor = 'rgba(162,215,255,1)';
+obj.bgcolor = 'rgba(69,91,108,1)';
 obj.text = "id, uuid, vault_id,\nname, description,\ncypher_string,\nowner_id";
 obj.textsize = 6;
 obj.draw_arrow = "none";
@@ -221,8 +245,8 @@ obj.pos.x = 52;
 obj.width = 14;
 obj.height = 44;
 obj.bgcolor = 'rgba(14,75,213,1)';
-obj.text = "Decrypt_Secret (uuid)";
-obj.textsize = 7;
+obj.text = "Decrypt_Secret";
+obj.textsize = 9;
 obj.draw_arrow = "none";
 obj.border_radius = "0";
 
@@ -316,7 +340,7 @@ obj.pos.y = 39;
 obj.pos.x = 110;
 obj.width = 10;
 obj.height = 2;
-obj.bgcolor = 'rgba(162,215,255,1)';
+obj.bgcolor = 'rgba(69,91,108,1)';
 obj.text = "string";
 obj.textsize = 6;
 obj.draw_arrow = "none";
@@ -353,8 +377,8 @@ obj.pos.x = 78;
 obj.width = 14;
 obj.height = 10;
 obj.bgcolor = 'rgba(14,75,213,1)';
-obj.text = "Decrypt \n(secret.cipher_string, key)";
-obj.textsize = 6;
+obj.text = "Decrypt ";
+obj.textsize = 9;
 obj.draw_arrow = "none";
 obj.border_radius = "0";
 
@@ -376,7 +400,7 @@ obj.pos.y = 53;
 obj.pos.x = 110;
 obj.width = 10;
 obj.height = 2;
-obj.bgcolor = 'rgba(162,215,255,1)';
+obj.bgcolor = 'rgba(69,91,108,1)';
 obj.text = "string";
 obj.textsize = 6;
 obj.draw_arrow = "none";
@@ -560,5 +584,425 @@ obj.bgcolor = 'rgba(0,188,255,0.39)';
 obj.text = "";
 obj.textsize = 7;
 obj.draw_arrow = "left_narrow";
+obj.border_radius = "0";
+
+
+obj = newRect(ObjectList.objects);
+obj.pos.y = 77;
+obj.pos.x = 74;
+obj.width = 52;
+obj.height = 18;
+obj.bgcolor = 'rgba(13,5,50,1)';
+obj.text = "   Backend.Profile";
+obj.textsize = 8;
+obj.draw_arrow = "none";
+obj.border_radius = "30";
+
+
+obj = newRect(ObjectList.objects);
+obj.pos.y = 77;
+obj.pos.x = 37;
+obj.width = 10;
+obj.height = 54;
+obj.bgcolor = 'rgba(13,5,50,1)';
+obj.text = "   User";
+obj.textsize = 8;
+obj.draw_arrow = "none";
+obj.border_radius = "30";
+
+
+obj = newRect(ObjectList.objects);
+obj.pos.y = 71;
+obj.pos.x = 37;
+obj.width = 70;
+obj.height = 4;
+obj.bgcolor = 'rgba(13,5,50,0)';
+obj.text = "Generate User Private Key at Login (session[\'secret_key'])";
+obj.textsize = 15;
+obj.draw_arrow = "none";
+obj.border_radius = "0";
+
+
+obj = newRect(ObjectList.objects);
+obj.pos.y = 77;
+obj.pos.x = 48;
+obj.width = 24;
+obj.height = 54;
+obj.bgcolor = 'rgba(13,5,50,1)';
+obj.text = "   Frontend";
+obj.textsize = 8;
+obj.draw_arrow = "none";
+obj.border_radius = "30";
+
+
+obj = newRect(ObjectList.objects);
+obj.pos.y = 82;
+obj.pos.x = 40;
+obj.width = 12;
+obj.height = 4;
+obj.bgcolor = 'rgba(0,188,255,1)';
+obj.text = "username\npassword";
+obj.textsize = 7;
+obj.draw_arrow = "right";
+obj.border_radius = "0";
+
+
+obj = newRect(ObjectList.objects);
+obj.pos.y = 82;
+obj.pos.x = 52;
+obj.width = 14;
+obj.height = 46;
+obj.bgcolor = 'rgba(14,75,213,1)';
+obj.text = "render_login_page";
+obj.textsize = 7;
+obj.draw_arrow = "none";
+obj.border_radius = "0";
+
+
+obj = newRect(ObjectList.objects);
+obj.pos.y = 82;
+obj.pos.x = 66;
+obj.width = 14;
+obj.height = 4;
+obj.bgcolor = 'rgba(0,188,255,1)';
+obj.text = "username\npassword";
+obj.textsize = 7;
+obj.draw_arrow = "right";
+obj.border_radius = "0";
+
+
+obj = newRect(ObjectList.objects);
+obj.pos.y = 81;
+obj.pos.x = 80;
+obj.width = 14;
+obj.height = 10;
+obj.bgcolor = 'rgba(14,75,213,1)';
+obj.text = "Authenticate";
+obj.textsize = 8;
+obj.draw_arrow = "none";
+obj.border_radius = "0";
+
+
+obj = newRect(ObjectList.objects);
+obj.pos.y = 80;
+obj.pos.x = 110;
+obj.width = 14;
+obj.height = 12;
+obj.bgcolor = 'rgba(0,188,255,1)';
+obj.text = "success: bool\nmessage: string\nreturn_code: int\npayload: [user_id]";
+obj.textsize = 6;
+obj.draw_arrow = "none";
+obj.border_radius = "5";
+
+
+obj = newRect(ObjectList.objects);
+obj.pos.y = 86;
+obj.pos.x = 112;
+obj.width = 10;
+obj.height = 2;
+obj.bgcolor = 'rgba(69,91,108,1)';
+obj.text = "int";
+obj.textsize = 6;
+obj.draw_arrow = "none";
+obj.border_radius = "5";
+
+
+obj = newRect(ObjectList.objects);
+obj.pos.y = 83;
+obj.pos.x = 94;
+obj.width = 16;
+obj.height = 2;
+obj.bgcolor = 'rgba(255,255,255,0.47)';
+obj.text = "";
+obj.textsize = 7;
+obj.draw_arrow = "right_narrow";
+obj.border_radius = "0";
+
+
+obj = newRect(ObjectList.objects);
+obj.pos.y = 88;
+obj.pos.x = 66;
+obj.width = 44;
+obj.height = 2;
+obj.bgcolor = 'rgba(0,188,255,0.39)';
+obj.text = "";
+obj.textsize = 7;
+obj.draw_arrow = "left_narrow";
+obj.border_radius = "0";
+
+
+obj = newRect(ObjectList.objects);
+obj.pos.y = 89;
+obj.pos.x = 52;
+obj.width = 14;
+obj.height = 2;
+obj.bgcolor = 'rgba(0,19,182,0.17)';
+obj.text = "user_id = payload";
+obj.textsize = 6;
+obj.draw_arrow = "none";
+obj.border_radius = "0";
+
+
+obj = newRect(ObjectList.objects);
+obj.pos.y = 87;
+obj.pos.x = 52;
+obj.width = 14;
+obj.height = 2;
+obj.bgcolor = 'rgba(116,182,0,1)';
+obj.text = "return_code != 200?";
+obj.textsize = 6;
+obj.draw_arrow = "none";
+obj.border_radius = "0";
+
+
+obj = newRect(ObjectList.objects);
+obj.pos.y = 87;
+obj.pos.x = 46;
+obj.width = 6;
+obj.height = 2;
+obj.bgcolor = 'rgba(255,255,255,0.47)';
+obj.text = "";
+obj.textsize = 6;
+obj.draw_arrow = "left_narrow";
+obj.border_radius = "0";
+
+
+obj = newRect(ObjectList.objects);
+obj.pos.y = 87;
+obj.pos.x = 40;
+obj.width = 6;
+obj.height = 2;
+obj.bgcolor = 'rgba(255,60,159,1)';
+obj.text = "error: 403";
+obj.textsize = 6;
+obj.draw_arrow = "none";
+obj.border_radius = "5";
+
+
+obj = newRect(ObjectList.objects);
+obj.pos.y = 102;
+obj.pos.x = 80;
+obj.width = 14;
+obj.height = 22;
+obj.bgcolor = 'rgba(14,75,213,1)';
+obj.text = "Login_user";
+obj.textsize = 8;
+obj.draw_arrow = "none";
+obj.border_radius = "0";
+
+
+obj = newRect(ObjectList.objects);
+obj.pos.y = 101;
+obj.pos.x = 66;
+obj.width = 14;
+obj.height = 4;
+obj.bgcolor = 'rgba(0,188,255,1)';
+obj.text = "username\npassword\nuser_id";
+obj.textsize = 6;
+obj.draw_arrow = "right";
+obj.border_radius = "0";
+
+
+obj = newRect(ObjectList.objects);
+obj.pos.y = 102;
+obj.pos.x = 94;
+obj.width = 12;
+obj.height = 2;
+obj.bgcolor = 'rgba(0,188,255,1)';
+obj.text = "password";
+obj.textsize = 7;
+obj.draw_arrow = "right";
+obj.border_radius = "0";
+
+
+obj = newRect(ObjectList.objects);
+obj.pos.y = 102;
+obj.pos.x = 106;
+obj.width = 20;
+obj.height = 8;
+obj.bgcolor = 'rgba(14,75,213,1)';
+obj.text = "Generate_user_private_key";
+obj.textsize = 8;
+obj.draw_arrow = "none";
+obj.border_radius = "0";
+
+
+obj = newRect(ObjectList.objects);
+obj.pos.y = 102;
+obj.pos.x = 126;
+obj.width = 14;
+obj.height = 2;
+obj.bgcolor = 'rgba(0,188,255,0.25)';
+obj.text = "self.secret_key";
+obj.textsize = 7;
+obj.draw_arrow = "left";
+obj.border_radius = "0";
+
+
+obj = newRect(ObjectList.objects);
+obj.pos.y = 105;
+obj.pos.x = 126;
+obj.width = 4;
+obj.height = 2;
+obj.bgcolor = 'rgba(255,255,255,0.47)';
+obj.text = "";
+obj.textsize = 7;
+obj.draw_arrow = "right_narrow";
+obj.border_radius = "0";
+
+
+obj = newRect(ObjectList.objects);
+obj.pos.y = 105;
+obj.pos.x = 130;
+obj.width = 10;
+obj.height = 4;
+obj.bgcolor = 'rgba(0,188,255,1)';
+obj.text = "user_private_key";
+obj.textsize = 6;
+obj.draw_arrow = "none";
+obj.border_radius = "5";
+
+
+obj = newRect(ObjectList.objects);
+obj.pos.y = 107;
+obj.pos.x = 94;
+obj.width = 36;
+obj.height = 2;
+obj.bgcolor = 'rgba(0,188,255,0.39)';
+obj.text = "";
+obj.textsize = 7;
+obj.draw_arrow = "left_narrow";
+obj.border_radius = "0";
+
+
+obj = newRect(ObjectList.objects);
+obj.pos.y = 115;
+obj.pos.x = 104;
+obj.width = 22;
+obj.height = 8;
+obj.bgcolor = 'rgba(59,75,56,1)';
+obj.text = "   Session";
+obj.textsize = 8;
+obj.draw_arrow = "none";
+obj.border_radius = "30";
+
+
+obj = newRect(ObjectList.objects);
+obj.pos.y = 107;
+obj.pos.x = 80;
+obj.width = 14;
+obj.height = 2;
+obj.bgcolor = 'rgba(0,19,182,0.17)';
+obj.text = "user_private_key = result";
+obj.textsize = 6;
+obj.draw_arrow = "none";
+obj.border_radius = "0";
+
+
+obj = newRect(ObjectList.objects);
+obj.pos.y = 118;
+obj.pos.x = 94;
+obj.width = 14;
+obj.height = 2;
+obj.bgcolor = 'rgba(0,188,255,1)';
+obj.text = "user_private_key";
+obj.textsize = 6;
+obj.draw_arrow = "right";
+obj.border_radius = "5";
+
+
+obj = newRect(ObjectList.objects);
+obj.pos.y = 118;
+obj.pos.x = 108;
+obj.width = 16;
+obj.height = 2;
+obj.bgcolor = 'rgba(69,91,108,1)';
+obj.text = "session[\'user_private_key']";
+obj.textsize = 6;
+obj.draw_arrow = "none";
+obj.border_radius = "5";
+
+
+obj = newRect(ObjectList.objects);
+obj.pos.y = 121;
+obj.pos.x = 62;
+obj.width = 18;
+obj.height = 2;
+obj.bgcolor = 'rgba(0,188,255,0.39)';
+obj.text = "";
+obj.textsize = 7;
+obj.draw_arrow = "left_narrow";
+obj.border_radius = "0";
+
+
+obj = newRect(ObjectList.objects);
+obj.pos.y = 121;
+obj.pos.x = 56;
+obj.width = 6;
+obj.height = 2;
+obj.bgcolor = 'rgba(35,142,41,1)';
+obj.text = "    OK";
+obj.textsize = 6;
+obj.draw_arrow = "none";
+obj.border_radius = "5";
+
+
+obj = newRect(ObjectList.objects);
+obj.pos.y = 124;
+obj.pos.x = 52;
+obj.width = 14;
+obj.height = 2;
+obj.bgcolor = 'rgba(0,19,182,0.17)';
+obj.text = "(...)";
+obj.textsize = 6;
+obj.draw_arrow = "none";
+obj.border_radius = "0";
+
+
+obj = newRect(ObjectList.objects);
+obj.pos.y = 124;
+obj.pos.x = 46;
+obj.width = 6;
+obj.height = 2;
+obj.bgcolor = 'rgba(0,188,255,0.39)';
+obj.text = "";
+obj.textsize = 7;
+obj.draw_arrow = "left_narrow";
+obj.border_radius = "0";
+
+
+obj = newRect(ObjectList.objects);
+obj.pos.y = 124;
+obj.pos.x = 38;
+obj.width = 8;
+obj.height = 2;
+obj.bgcolor = 'rgba(35,142,41,1)';
+obj.text = "Home page";
+obj.textsize = 6;
+obj.draw_arrow = "none";
+obj.border_radius = "5";
+
+
+obj = newRect(ObjectList.objects);
+obj.pos.y = 29;
+obj.pos.x = 92;
+obj.width = 12;
+obj.height = 2;
+obj.bgcolor = 'rgba(0,188,255,0.25)';
+obj.text = "self.secret_key";
+obj.textsize = 7;
+obj.draw_arrow = "left";
+obj.border_radius = "0";
+
+
+obj = newRect(ObjectList.objects);
+obj.pos.y = 121;
+obj.pos.x = 80;
+obj.width = 14;
+obj.height = 2;
+obj.bgcolor = 'rgba(0,19,182,0.17)';
+obj.text = "(...)";
+obj.textsize = 6;
+obj.draw_arrow = "none";
 obj.border_radius = "0";
 
